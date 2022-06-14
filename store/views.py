548a442c93139382d2f73c8153de3828b10bef89
chakrapani710 <chakraa97@gmail.com>
2022-06-14 14:12:47 +0530

    Sample Commit

---
+++ store/views.py
@@ -0,0 +1,39 @@
+from itertools import product
+from multiprocessing import context
+import re
+from unicodedata import category
+from django.shortcuts import redirect, render
+from .models import *
+from django.contrib import messages
+# Create your views here.
+
+def home(request):
+    return render(request,"store/index.html")
+
+def collections(request):
+    category = Category.objects.filter(status=0)
+    context = {'category':category}
+    return render(request,"store/collections.html",context)
+
+def collectionsview(request, slug):
+    if(Category.objects.filter(slug=slug, status=0)):
+        products=Product.objects.filter(category__slug=slug)
+        category= Category.objects.filter(slug=slug).first()
+        context= {'products':products,'category':category}
+        return render(request,"store/products/index.html",context)
+    else:
+        messages.warning(request,"No such category found")
+        return redirect('collections')
+ 
+def productview(request, cate_slug, prod_slug):
+    if(Category.objects.filter(slug=cate_slug, status=0)):
+        if(Product.objects.filter(slug=prod_slug, status=0)):
+            products= (Product.objects.filter(slug=prod_slug, status=0)).first
+            context = {'products':products}
+        else:
+            messages.error(request,"No such Product found")
+            return redirect('collections')
+    else:
+        messages.error(request,"No such category found")
+        return redirect('collections')
+    return render(request,"store/products/view.html",context)
